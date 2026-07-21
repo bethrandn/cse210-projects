cf176ce49b43b8e88d2cc7961b3ed6e9850c35a5
bethrandn <bnwankwo1@byupathway.edu>
2026-07-21 19:04:34 +0100

    Complete Week 3 Fraction learning activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        
+        Fraction first = new Fraction();
+
+        Console.WriteLine(first.GetFractionString());   
+        Console.WriteLine(first.GetDecimalValue());     
+
+        
+        Fraction second = new Fraction(5);
+        Console.WriteLine(second.GetFractionString());  
+        Console.WriteLine(second.GetDecimalValue());    
+
+        
+        Fraction third = new Fraction(3, 4);
+        Console.WriteLine(third.GetFractionString());   
+        Console.WriteLine(third.GetDecimalValue());
+
+        Fraction four = new Fraction();
+        four.SetTop(1);
+        four.SetBottom(3);   
+
+        Console.WriteLine(four.GetTop()); 
+        Console.WriteLine(four.GetBottom());
+
+        Console.WriteLine(four.GetFractionString());
+        Console.WriteLine(four.GetDecimalValue()); 
+
     }
 }
